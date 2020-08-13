b8a2d88249fbe0ce02ebeba869b286476567ac8f
bbastanza <b.bastanza@gmail.com>
2020-08-13 12:57:18 -0700

    commit scoring/organize to objects

--- script.js
+++ script.js
@@ -1,99 +1,111 @@
 const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
-const apple = new Image();
-apple.src = "images/apple.png";
-const snakeHead = new Image();
-snakeHead.src = "images/olaf2.png";
-const snakeBodyPiece = new Image();
-snakeBodyPiece.src = "images/snowball.png";
-let appleX = 480;
-let appleY = 30;
-let snakeHeadX = 30;
-let snakeHeadY = 30;
-let moveAmount = 15;
 
-apple.onload = function () {
-    ctx.drawImage(apple, appleX, appleY);
+window.onload = () => {
+    drawApple();
+    gamePlay();
+    // put highscore on screen from local storage
+};
+
+const SCORING = {
+    score: 0,
+    highScore: 0,
+};
+
+const SNAKE = {
+    body: [{ x: 30, y: 30 }],
+    direction: "RIGHT",
+    newDirection: "RIGHT",
+    moveAmount: 3.75,
+};
+
+const APPLE = {
+    x: "",
+    y: "",
 };
 
 function checkAppleCollision() {
-    if (snakeHeadX === appleX && snakeHeadY === appleY) {
-        appleX = Math.floor(23 * Math.random()) * 30 + 30;
-        appleY = Math.floor(14 * Math.random()) * 30 + 30;
-        //if apple position does not equal body or head position of the snake draw image///else rerun
-        ctx.drawImage(apple, appleX, appleY);
+    if (SNAKE.body[0].x === APPLE.x && SNAKE.body[0].y === APPLE.y) {
+        drawApple();
+        SCORING.score++;
+
+        if (SCORING.score > SCORING.highScore) {
+            SCORING.highScore = SCORING.score;
+            document.getElementById("high-score").textContent = `High Score: ${SCORING.highScore}`;
+        }
+        document.getElementById("score").textContent = `Score: ${SCORING.score}`;
     }
 }
 
-const framesPerSecond = 20;
-let negativeAxis = false;
-let direction = "RIGHT";
+function drawApple() {
+    const apple = new Image();
+    apple.src = "images/apple.png";
+    apple.onload = () => {
+        APPLE.x = Math.floor(23 * Math.random()) * 30 + 30;
+        APPLE.y = Math.floor(14 * Math.random()) * 30 + 30;
+        //if apple position does not equal body or head position of the snake draw image///else rerun
+        ctx.drawImage(apple, APPLE.x, APPLE.y);
+    };
+}
 
-snakeHead.onload = function () {
-    setInterval(function () {
-        ctx.clearRect(snakeHeadX, snakeHeadY, 30, 30);
+function gamePlay() {
+    const snakeHeadImage = new Image();
+    snakeHeadImage.src = "images/olaf2.png";
+    const snakeBodyPiece = new Image();
+    snakeBodyPiece.src = "images/snowball.png";
+    snakeHeadImage.onload = function () {
+        const framesPerSecond = 60;
+        setInterval(function () {
+            ctx.clearRect(SNAKE.body[0].x - 3, SNAKE.body[0].y - 3, 36, 36);
 
-        if (direction === "RIGHT" || direction === "LEFT") {
-            snakeHeadX = updateAxis(snakeHeadX);
-        } else {
-            snakeHeadY = updateAxis(snakeHeadY);
-        }
+            if (SNAKE.body[0].x % 30 === 0 && SNAKE.body[0].y % 30 === 0) {
+                SNAKE.direction = SNAKE.newDirection;
+            }
 
-        ctx.drawImage(snakeHead, snakeHeadX, snakeHeadY);
-        checkAppleCollision();
-    }, 1000 / framesPerSecond);
-};
+            if (SNAKE.direction === "RIGHT") {
+                SNAKE.body[0].x += SNAKE.moveAmount;
+            }
+            if (SNAKE.direction === "LEFT") {
+                SNAKE.body[0].x -= SNAKE.moveAmount;
+            }
+            if (SNAKE.direction === "DOWN") {
+                SNAKE.body[0].y += SNAKE.moveAmount;
+            }
+            if (SNAKE.direction === "UP") {
+                SNAKE.body[0].y -= SNAKE.moveAmount;
+            }
 
-function updateAxis(position) {
-    if (!negativeAxis) {
-        position += moveAmount;
-        return position;
-    } else {
-        position -= moveAmount;
-        return position;
-    }
+            ctx.drawImage(snakeHeadImage, SNAKE.body[0].x, SNAKE.body[0].y);
+            checkAppleCollision();
+            checkBorderCollision();
+            // check snake collision
+        }, 1000 / framesPerSecond);
+    };
 }
 
 document.addEventListener("keydown", function (e) {
     if (e.key === "Right" || e.key === "ArrowRight") {
-        if (snakeHeadX % 30 === 15) {
-            snakeHeadX += 15;
-            ctx.clearRect(snakeHeadX - 15, snakeHeadY, 30, 30);
-        }
-        direction = "RIGHT";
-        negativeAxis = false;
+        SNAKE.newDirection = "RIGHT";
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+        SNAKE.newDirection = "LEFT";
+    } else if (e.key === "Up" || e.key === "ArrowUp") {
+        SNAKE.newDirection = "UP";
+    } else if (e.key === "Down" || e.key === "ArrowDown") {
+        SNAKE.newDirection = "DOWN";
+    } else if (e.keyCode === 32) {
+        alert("Pause");
     }
 });
 
-document.addEventListener("keydown", function (e) {
-    if (e.key === "Left" || e.key === "ArrowLeft") {
-        if (snakeHeadX % 30 === 15) {
-            snakeHeadX -= 15;
-            ctx.clearRect(snakeHeadX + 15, snakeHeadY, 30, 30);
-        }
-        direction = "LEFT";
-        negativeAxis = true;
+function checkBorderCollision() {
+    if (
+        SNAKE.body[0].x > canvas.width - 30 ||
+        SNAKE.body[0].x < 0 ||
+        SNAKE.body[0].y > canvas.height - 30 ||
+        SNAKE.body[0].y < 0
+    ) {
+        alert(`You Hit A Wall`);
     }
-});
+}
 
-document.addEventListener("keydown", function (e) {
-    if (e.key === "Up" || e.key === "ArrowUp") {
-        if (snakeHeadY % 30 === 15) {
-            snakeHeadY -= 15;
-            ctx.clearRect(snakeHeadX, snakeHeadY + 15, 30, 30);
-        }
-        direction = "UP";
-        negativeAxis = true;
-    }
-});
-
-document.addEventListener("keydown", function (e) {
-    if (e.key === "Down" || e.key === "ArrowDown") {
-        if (snakeHeadY % 30 === 15) {
-            snakeHeadY += 15;
-            ctx.clearRect(snakeHeadX, snakeHeadY - 15, 30, 30);
-        }
-        direction = "DOWN";
-        negativeAxis = false;
-    }
-});
+// game over function include local storage of high score
