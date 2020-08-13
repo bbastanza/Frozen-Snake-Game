7ffc09059bd759fab0db1bae250d3c5548aec0e3
bbastanza <b.bastanza@gmail.com>
2020-08-12 17:09:47 -0700

    commit tinkering/randomizing apple location on x axis

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
-window.onload = function () {
-    let apple = new Image();
-    apple.src = "images/apple.png";
+const canvas = document.getElementById("game-canvas");
+const ctx = canvas.getContext("2d");
+const apple = new Image();
+apple.src = "images/apple.png";
+const snakeHead = new Image();
+snakeHead.src = "images/olaf2.png";
+const snakeBodyPiece = new Image();
+snakeBodyPiece.src = "images/snowball.png";
+let appleX = 480;
+let appleY = 30;
+let snakeHeadX = 30;
+let snakeHeadY = 30;
+let moveX = 7.5;
 
-    let canvas = document.getElementById("game-canvas");
-    let ctx = canvas.getContext("2d");
-    appleX = Math.floor(34 * Math.random()) * 15 + 20;
-    appleY = Math.floor(24 * Math.random()) * 20 + 20;
-
+apple.onload = function () {
     ctx.drawImage(apple, appleX, appleY);
 };
-let canvas = document.getElementById("game-canvas");
-let ctx = canvas.getContext("2d");
-appleX = Math.floor(23 * Math.random()) * 30 + 30;
-appleY = Math.floor(14 * Math.random()) * 30 + 30;
-olafX = 30;
-olafY = 30;
-snowballX = Math.floor(23 * Math.random()) * 30 + 30;
-snowballY = Math.floor(14 * Math.random()) * 30 + 30;
-let moveX = 1;
-let snowball = new Image();
-snowball.src = "images/snowball.png";
-let apple = new Image();
-apple.src = "images/apple.png";
 
-setInterval(function () {
-    let olaf = new Image();
-    olaf.src = "images/olaf2.png";
-    let olafX = updateX(olafX, moveX);
-    olaf.onload = function () {
-        ctx.drawImage(olaf, olafX, 0, canvas.width, canvas.height);
-    };
-}, 100);
+function checkAppleCollision() {
+    if (snakeHeadX === appleX && snakeHeadY === appleY) {
+        appleX = Math.floor(23 * Math.random()) * 30 + 30;
+        // appleY = Math.floor(14 * Math.random()) * 30 + 30;
+        //if apple position does not equal body or head position of the snake draw image///else rerun
+        ctx.drawImage(apple, appleX, appleY);
+    }
+}
 
-function updateX(x, dx) {
-    x += dx;
-    return x;
+const framesPerSecond = 30;
+
+snakeHead.onload = function () {
+    setInterval(function () {
+        ctx.clearRect(snakeHeadX, snakeHeadY, 30, 30);
+        snakeHeadX = updateAxis(snakeHeadX);
+        ctx.drawImage(snakeHead, snakeHeadX, snakeHeadY);
+        checkAppleCollision();
+    }, 1000 / framesPerSecond);
+};
+
+function updateAxis(position) {
+    position += moveX;
+    if (position > canvas.width - 25 || position < -5) {
+        moveX = -moveX;
+    }
+    return position;
 }
