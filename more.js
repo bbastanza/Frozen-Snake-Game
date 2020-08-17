6fd7b63921edfbd130d5233bfd866ec38c9ee738
bbastanza <b.bastanza@gmail.com>
2020-08-17 15:33:18 -0700

    commit simplify

--- more.js
+++ more.js
@@ -1,10 +1,10 @@
 function gamePlay() {
-    const snakeHeadImage = new Image();
-    snakeHeadImage.src = "images/olaf2.png";
-    const snakeBodyPiece = new Image();
-    snakeBodyPiece.src = "images/snowball.png";
-    const apple = new Image();
-    apple.src = "images/apple.png";
+    const HEAD_IMAGE = new Image();
+    HEAD_IMAGE.src = "images/olaf2.png";
+    const BODY_IMAGE = new Image();
+    BODY_IMAGE.src = "images/snowball.png";
+    const APPLE_IMAGE = new Image();
+    APPLE_IMAGE.src = "images/apple.png";
 
     setInterval(function () {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -15,136 +15,50 @@ function gamePlay() {
 
         if (SNAKE.direction === "RIGHT") {
             SNAKE.body[0].x += SNAKE.moveAmount;
-            for (let i = 1; i < SNAKE.body.length; i++) {
-                if (SNAKE.body[i].y > SNAKE.body[i - 1].y) {
-                    SNAKE.body[i].y -= SNAKE.moveAmount;
-                } else if (SNAKE.body[i].y < SNAKE.body[i - 1].y) {
-                    SNAKE.body[i].y += SNAKE.moveAmount;
-                } else if (SNAKE.body[i].x > SNAKE.body[i - 1].x) {
-                    SNAKE.body[i].x -= SNAKE.moveAmount;
-                } else if (SNAKE.body[i].x < SNAKE.body[i - 1].x) {
-                    SNAKE.body[i].x += SNAKE.moveAmount;
-                }
-
-                ctx.drawImage(snakeBodyPiece, SNAKE.body[i].x, SNAKE.body[i].y);
-            }
         }
+
         if (SNAKE.direction === "LEFT") {
             SNAKE.body[0].x -= SNAKE.moveAmount;
-            for (let i = 1; i < SNAKE.body.length; i++) {
-                if (SNAKE.body[i].y > SNAKE.body[i - 1].y) {
-                    SNAKE.body[i].y -= SNAKE.moveAmount;
-                } else if (SNAKE.body[i].y < SNAKE.body[i - 1].y) {
-                    SNAKE.body[i].y += SNAKE.moveAmount;
-                } else if (SNAKE.body[i].x > SNAKE.body[i - 1].x) {
-                    SNAKE.body[i].x -= SNAKE.moveAmount;
-                } else if (SNAKE.body[i].x < SNAKE.body[i - 1].x) {
-                    SNAKE.body[i].x += SNAKE.moveAmount;
-                }
-
-                ctx.drawImage(snakeBodyPiece, SNAKE.body[i].x, SNAKE.body[i].y);
-            }
         }
+
         if (SNAKE.direction === "DOWN") {
             SNAKE.body[0].y += SNAKE.moveAmount;
-            for (let i = 1; i < SNAKE.body.length; i++) {
-                if (SNAKE.body[i].x > SNAKE.body[i - 1].x) {
-                    SNAKE.body[i].x -= SNAKE.moveAmount;
-                } else if (SNAKE.body[i].x < SNAKE.body[i - 1].x) {
-                    SNAKE.body[i].x += SNAKE.moveAmount;
-                } else if (SNAKE.body[i].y > SNAKE.body[i - 1].y) {
-                    SNAKE.body[i].y -= SNAKE.moveAmount;
-                } else if (SNAKE.body[i].y < SNAKE.body[i - 1].y) {
-                    SNAKE.body[i].y += SNAKE.moveAmount;
-                }
-                ctx.drawImage(snakeBodyPiece, SNAKE.body[i].x, SNAKE.body[i].y);
-            }
-        }
-        if (SNAKE.direction === "UP") {
-            SNAKE.body[0].y -= SNAKE.moveAmount;
-            for (let i = 1; i < SNAKE.body.length; i++) {
-                if (SNAKE.body[i].x > SNAKE.body[i - 1].x) {
-                    SNAKE.body[i].x -= SNAKE.moveAmount;
-                } else if (SNAKE.body[i].x < SNAKE.body[i - 1].x) {
-                    SNAKE.body[i].x += SNAKE.moveAmount;
-                } else if (SNAKE.body[i].y > SNAKE.body[i - 1].y) {
-                    SNAKE.body[i].y -= SNAKE.moveAmount;
-                } else if (SNAKE.body[i].y < SNAKE.body[i - 1].y) {
-                    SNAKE.body[i].y += SNAKE.moveAmount;
-                }
-                ctx.drawImage(snakeBodyPiece, SNAKE.body[i].x, SNAKE.body[i].y);
-            }
         }
 
-        ctx.drawImage(apple, APPLE.x, APPLE.y);
-        ctx.drawImage(snakeHeadImage, SNAKE.body[0].x, SNAKE.body[0].y);
+        if (SNAKE.direction === "UP") {
+            SNAKE.body[0].y -= SNAKE.moveAmount;
+        }
+
+        for (let i = SNAKE.body.length; i < SNAKE.body.length + 1; i--) {
+            const child = SNAKE.body[i];
+            const parent = SNAKE.body[i - 1];
+
+            if (child.y > parent.y) {
+                child.y -= SNAKE.moveAmount;
+            } else if (child.y < parent.y) {
+                child.y += SNAKE.moveAmount;
+            } else if (child.x > parent.x && parent.y % 30 === 0) {
+                child.x -= SNAKE.moveAmount;
+            } else if (child.x < parent.x && parent.y % 30 === 0) {
+                child.x += SNAKE.moveAmount;
+            }
+
+            ctx.drawImage(BODY_IMAGE, child.x, child.y);
+        }
+
+        ctx.drawImage(APPLE_IMAGE, APPLE.x, APPLE.y);
+        ctx.drawImage(HEAD_IMAGE, SNAKE.body[0].x, SNAKE.body[0].y);
         checkAppleCollision();
-        checkBorderCollision();
-        checkBodyCollision();
-    }, 1000 / framesPerSecond);
+        checkLosingCollision();
+    }, 1000 / FRAMES_PER_SECOND);
 }
 
 function addSnakeBodyPart() {
     let newBodyX = APPLE.x;
     let newBodyY = APPLE.y;
     // time for each body piece to move a grid length
-    let snakeBodyTime = (1000 / framesPerSecond) * (gridSize / SNAKE.moveAmount);
+    let snakeBodyTime = (1000 / FRAMES_PER_SECOND) * (GRID_SIZE / SNAKE.moveAmount);
     setTimeout(() => {
         SNAKE.body.push({ x: newBodyX, y: newBodyY });
     }, snakeBodyTime * SNAKE.body.length);
 }
-
-function checkBodyCollision() {
-    for (let i = 1; i < SNAKE.body.length; i++) {
-        const bodyElement = SNAKE.body[i];
-        if (SNAKE.body[0].x === bodyElement.x && SNAKE.body[0].y === bodyElement.y) {
-            gameOver();
-        }
-    }
-}
-
-////////////////////////////////////////////////////////////////////
-// for (let i = 1; i < SNAKE.body.length; i++) {
-//     if (SNAKE.body[i].y > SNAKE.body[i - 1].y) {
-//         SNAKE.body[i].y -= SNAKE.moveAmount;
-//     } else if (SNAKE.body[i].y < SNAKE.body[i - 1].y) {
-//         SNAKE.body[i].y += SNAKE.moveAmount;
-//     } else if (SNAKE.body[i].x > SNAKE.body[i - 1].x) {
-//         SNAKE.body[i].x -= SNAKE.moveAmount;
-//     } else if (SNAKE.body[i].x < SNAKE.body[i - 1].x) {
-//         SNAKE.body[i].x += SNAKE.moveAmount;
-//     }
-
-//     ctx.drawImage(snakeBodyPiece, SNAKE.body[i].x, SNAKE.body[i].y);
-// }
-////////////////////////////////////////////////////////////////////////
-// function verticalMovement() {
-//     for (let i = 1; i < SNAKE.body.length; i++) {
-//         if (SNAKE.body[i].y > SNAKE.body[i - 1].y) {
-//             SNAKE.body[i].y -= SNAKE.moveAmount;
-//         } else if (SNAKE.body[i].y < SNAKE.body[i - 1].y) {
-//             SNAKE.body[i].y += SNAKE.moveAmount;
-//         } else if (SNAKE.body[i].x > SNAKE.body[i - 1].x) {
-//             SNAKE.body[i].x -= SNAKE.moveAmount;
-//         } else if (SNAKE.body[i].x < SNAKE.body[i - 1].x) {
-//             SNAKE.body[i].x += SNAKE.moveAmount;
-//         }
-
-//         ctx.drawImage(snakeBodyPiece, SNAKE.body[i].x, SNAKE.body[i].y);
-//     }
-// }
-
-// function horizontalMovement() {
-//     for (let i = 1; i < SNAKE.body.length; i++) {
-//         if (SNAKE.body[i].x > SNAKE.body[i - 1].x) {
-//             SNAKE.body[i].x -= SNAKE.moveAmount;
-//         } else if (SNAKE.body[i].x < SNAKE.body[i - 1].x) {
-//             SNAKE.body[i].x += SNAKE.moveAmount;
-//         } else if (SNAKE.body[i].y > SNAKE.body[i - 1].y) {
-//             SNAKE.body[i].y -= SNAKE.moveAmount;
-//         } else if (SNAKE.body[i].y < SNAKE.body[i - 1].y) {
-//             SNAKE.body[i].y += SNAKE.moveAmount;
-//         }
-//         ctx.drawImage(snakeBodyPiece, SNAKE.body[i].x, SNAKE.body[i].y);
-//     }
-// }
--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
-const scoreDisplay = document.getElementById("score");
-const highScoreDisplay = document.getElementById("high-score");
 
-const framesPerSecond = 60;
-const gridSize = 30;
+const SCORE_DISPLAY = document.getElementById("score");
+const HIGH_SCORE_DISPLAY = document.getElementById("high-score");
+
+const FRAMES_PER_SECOND = 60;
+const GRID_SIZE = 30;
+
 window.onload = () => {
     drawRandomApple();
     gamePlay();
@@ -33,27 +35,26 @@ function checkAppleCollision() {
         SCORING.score++;
         if (SCORING.score > SCORING.highScore) {
             SCORING.highScore = SCORING.score;
-            highScoreDisplay.textContent = `High Score: ${SCORING.highScore}`;
+            HIGH_SCORE_DISPLAY.textContent = `High Score: ${SCORING.highScore}`;
         }
-        scoreDisplay.textContent = `Score: ${SCORING.score}`;
+        SCORE_DISPLAY.textContent = `Score: ${SCORING.score}`;
         addSnakeBodyPart();
         drawRandomApple();
     }
 }
 
 function drawRandomApple() {
-    const apple = new Image();
-    apple.src = "images/apple.png";
-    apple.onload = () => {
+    const APPLE_IMAGE = new Image();
+    APPLE_IMAGE.src = "images/apple.png";
+    APPLE_IMAGE.onload = () => {
         APPLE.x = Math.floor(23 * Math.random()) * 30 + 30;
         APPLE.y = Math.floor(14 * Math.random()) * 30 + 30;
         //if apple position does not equal body or head position of the snake draw image///else rerun ////while loop////
-        ctx.drawImage(apple, APPLE.x, APPLE.y);
+        ctx.drawImage(APPLE_IMAGE, APPLE.x, APPLE.y);
     };
 }
 
 document.addEventListener("keydown", function (e) {
-    // prevent left if right, right if left, up if down, down if up
     if (e.key === "Right" || e.key === "ArrowRight") {
         if (SNAKE.direction !== "LEFT") {
             SNAKE.newDirection = "RIGHT";
@@ -75,7 +76,14 @@ document.addEventListener("keydown", function (e) {
     }
 });
 
-function checkBorderCollision() {
+function checkLosingCollision() {
+    for (let i = 1; i < SNAKE.body.length; i++) {
+        const BODY_PART = SNAKE.body[i];
+        if (SNAKE.body[0].x === BODY_PART.x && SNAKE.body[0].y === BODY_PART.y) {
+            gameOver();
+        }
+    }
+
     if (
         SNAKE.body[0].x > canvas.width - 30 ||
         SNAKE.body[0].x < 0 ||
@@ -96,7 +104,7 @@ function gameOver() {
     SNAKE.newDirection = "RIGHT";
     SNAKE.moveAmount = 3.75;
     drawRandomApple();
-    scoreDisplay.textContent = `Score: ${SCORING.score}`;
+    SCORE_DISPLAY.textContent = `Score: ${SCORING.score}`;
 }
 
 // game over function include local storage of high score
